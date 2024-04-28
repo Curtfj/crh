21132d5bfcff5308efd148ce1cba7a747f64d070
stealheadone <418364620@qq.com>
2024-04-28 10:35:05 +0800

    启用过滤器

--- src/main/java/cn/zczj/hq/HqApplication.java
+++ src/main/java/cn/zczj/hq/HqApplication.java
@@ -2,8 +2,10 @@ package cn.zczj.hq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan({"cn.zczj.hq.filter"})
 public class HqApplication {
 
 	public static void main(String[] args) {
